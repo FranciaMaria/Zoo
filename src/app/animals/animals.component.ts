d741795156846ba224bf91c37521fbfe0b5cf4e5
FranciaMaria <mariafrancia88@gmail.com>
2017-10-10 19:26:21 +0200

    task 1

---
+++ src/app/animals/animals.component.ts
@@ -0,0 +1,43 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-animals',
+  templateUrl: './animals.component.html'
+})
+export class AnimalsComponent {
+
+	AnimalList:Array<Object> = [
+
+		{
+ 			vrsta: 'macka',
+ 			ime: 'Lulu',
+ 			datumRodjenja: '1976-02-12'
+		},
+
+		{
+ 			vrsta: 'konj',
+ 			ime: 'Spirit',
+ 			datumRodjenja: '1990-07-02'
+		},
+
+		{
+ 			vrsta: 'slon',
+ 			ime: 'Kika',
+ 			datumRodjenja: '1999-06-11'
+		},
+
+		{
+ 			vrsta: 'zmija',
+ 			ime: 'Su',
+ 			datumRodjenja: '2010-05-09'
+		},
+
+		{
+ 			vrsta: 'ptica',
+ 			ime: 'Po',
+ 			datumRodjenja: '2000-08-15'
+		}
+
+	];
+
+}
